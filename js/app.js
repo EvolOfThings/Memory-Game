7bd6d6a018847f78c76e34491c82565afaea2532
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-10-22 18:43:54 +0530

    openedCards function

--- js/app.js
+++ js/app.js
@@ -51,20 +51,31 @@ shuffledCards = shuffle(allCards);
 // shortcuts variables
 deck = $('.deck');
 
+let openedCards = [];
+
 
 
 // click event listener to the card elements.
 deck.on('click', '.card', function() {
-  let opened = $(this).addClass('open show');
+
+let opened = $(this).addClass('open show');
 
 // disable the card from being clicked again after it is opened
   if($(this).hasClass('open')) {
     $(this).prop('disabled', true);
+  } else {
+    return opened ;
   }
 
   // push the cards to openedCards array
+openedCards.push(...opened.children()); // spread is used on opened array to avoid nesting
+console.log(openedCards);
+
 
   // compare the clicked cards array
+
+
+
 
   // if has same children classes  then add match class
 });
@@ -105,7 +116,7 @@ deck.on('click', '.card', function() {
  */
 
 
-let openedCards = [];
+
 
 
 
