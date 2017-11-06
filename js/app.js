501813bc920e9c856a897eb42f3fd76d24003e0c
RishadYamnoor <rishad.yamnoor@gmail.com>
2017-11-06 11:06:25 +0530

    cards generator function

--- js/app.js
+++ js/app.js
@@ -38,14 +38,39 @@ function shuffle(array) {
   return array;
 }
 
+shuffledCards = shuffle(allCards);
+
 /*
  * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
+ * DONE  - shuffle the list of cards using the provided "shuffle" method
+ * DONE  - loop through each card and create its HTML
+ * DONE  - add each card's HTML to the page
  */
 
-shuffledCards = shuffle(allCards);
+//generated li and i element for each card
+
+generateCards = () => {
+  for(const element of shuffledCards) {
+    const deckList = $('ul.deck');
+    let li = $('<li/>').addClass('card').appendTo(deckList);
+    let iTag = $('<i/>').addClass(element).appendTo(li);
+  }
+};
+
+generateCards();
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ * DONE - display the card's symbol (put this functionality in another function that you call from this one)
+ * DONE - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * DONE - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
+
 
 
 // shortcuts variables
@@ -55,7 +80,7 @@ let deck = $('.deck'),
 
 
 
-//In fat arrow functions, this never gets bound to a new value
+//Reminder: In fat arrow functions, this never gets bound to a new value
 
 // click event listener to the card elements.
 deck.on('click', '.card', (event) => {
@@ -142,16 +167,6 @@ function removeCardsFromList() {
 }
 
 
-/*
- * set up the event listener for a card. If a card is clicked:
- * DONE - display the card's symbol (put this functionality in another function that you call from this one)
- * DONE - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
 
 
 
@@ -164,6 +179,8 @@ function removeCardsFromList() {
 //                TO CODE
 // 1. movecounter
 // 2. all cards matched - win modal
+// 3. restart function
+
 
 
 
